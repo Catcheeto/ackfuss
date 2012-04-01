cf76212990e676351ed5c4fa5a24f29fbcc0e1e8
Matthew Goff <matt@goff.cc>
2012-04-01 14:59:42 -0500

    Splitting 'classes' into their own actual classes.

---
+++ src/baseclass.cpp
@@ -0,0 +1,14 @@
+/***************************************************************************
+ * _/_/_/_/  _/    _/  _/_/_/_/ _/_/_/_/ AckFUSS is modified ACK!MUD 4.3.1 *
+ * _/        _/    _/  _/       _/       copyright Matt Goff (Kline) 2008  *
+ * _/_/      _/    _/  _/_/_/_/ _/_/_/_/                                   *
+ * _/        _/    _/        _/       _/ Support for this code is provided *
+ * _/        _/_/_/_/  _/_/_/_/ _/_/_/_/ at www.ackmud.net -- check it out!*
+ ***************************************************************************/
+
+#include "h/globals.h"
+
+#ifndef DEC_BASECLASS_H
+#include "h/baseclass.h"
+#endif
+
